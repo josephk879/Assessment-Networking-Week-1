651344a323188519425971d7cdb15aaddd13b35b
Jo Kellock <trainee.jo.kellock@sigmalabs.co.uk>
2026-07-03 15:22:01 +0100

    first attempt at task 1

--- postcode_cli.py
+++ postcode_cli.py
@@ -1,6 +1,51 @@
 """A CLI application for interacting with the Postcode API."""
 
+"""#### Arguments
+When run, the script should accept the following arguments:
+1. `--mode`/`-m` : a **required** argument that accepts only the values `validate` and `complete`
+2. `postcode` : a **required** argument that accepts a string
+
+- `python3 postcode_cli.py --mode validate "FN1 MR2"`
+- `python3 postcode_cli.py -m validate "FN3 XF5"`
+- `python3 postcode_cli.py --mode complete "FN1"`
+- `python3 postcode_cli.py -m complete "FN3"`
+
+#### Validation mode
+When the script is run and the `--mode`/`-m` argument has the value of `validate`, the tool should check if the 
+provided postcode is valid.
+If the postcode is valid, the tool should output `'[postcode] is a valid postcode.'` only.
+If the postcode is not valid, the tool should output `'[postcode] is not a valid postcode.'` only.
+Regardless of how they are entered, postcodes should be checked/displayed as **uppercase-only** strings with no trailing spaces.
+
+#### Completion mode
+When the script is run and the `--mode`/`-m` argument has the value of `complete`, the tool should display valid 
+postcodes that would complete the provided partial postcode.
+Each valid completion should be displayed on its own line, as shown below. A maximum of **5** possible completions should be shown.
+
+```
+TN12 0AA
+TN12 0AB
+TN12 0AD
+TN12 0AE
+TN12 0AF
+```
+If there are no valid completions, the tool should display only `'No matches for [postcode].'`"""
+
+
 from argparse import ArgumentParser
+def postcode_argparse() -> tuple[str, str]:
+    parser = argparse.ArgumentParser(description="Airport departure board")
+    parser.add_argument(
+        "--airport", help="Airport name to search for", required=True)
+    parser.add_argument(
+        "--export", "-e",
+        help="Export to HTML, JSON, or both (default: both)",
+        choices=["html", "json", "both"],
+        default=None,
+        required=False
+    )
+    args = parser.parse_args()
+    return args.airport, args.export
 
 
 if __name__ == "__main__":
--- postcode_functions.py
+++ postcode_functions.py
@@ -6,6 +6,7 @@ import json
 
 CACHE_FILE = "./postcode_cache.json"
 
+
 def load_cache() -> dict:
     """Loads the cache from a file and converts it from JSON to a dictionary."""
     # This function is used in Task 3, you can ignore it for now.
@@ -18,18 +19,66 @@ def save_cache(cache: dict):
     ...
 
 
-
 def validate_postcode(postcode: str) -> bool:
-    pass
+
+    if not isinstance(postcode, str):
+        raise TypeError("Function expects a string.")
+
+    res = req.get(f"https://api.postcodes.io/postcodes/{postcode}/validate")
+
+    if res.status_code == 500:
+        raise req.RequestException("Unable to access API.")
+
+    if res.status_code == 200:
+
+    return False
 
 
 def get_postcode_for_location(lat: float, long: float) -> str:
-    pass
+    if not isinstance(lat, float) or not isinstance(long, float):
+        raise TypeError("Function expects two floats.")
+
+    res = req.get(f"https://api.postcodes.io/postcodes?lon={long}&lat={lat}")
+
+    if res.status_code == 200:
+        res_json = res.json()["result"]
+        if res_json == None:
+            raise ValueError("No relevant postcode found.")
+        return res_json[0]["postcode"]
+
+    if res.status_code == 500:
+        raise req.RequestException("Unable to access API.")
 
 
 def get_postcode_completions(postcode_start: str) -> list[str]:
-    pass
+    if not isinstance(postcode_start, str):
+        raise TypeError("Function expects a string.")
+    res = req.get(
+        f"https://api.postcodes.io/postcodes/{postcode_start}/autocomplete")
+
+    if res.status_code == 200:
+        return res.json()["result"]
+
+    if res.status_code == 500:
+        raise req.RequestException("Unable to access API.")
 
 
 def get_postcodes_details(postcodes: list[str]) -> dict:
-    pass
+
+    if not isinstance(postcodes, list):
+        raise TypeError("Function expects a list of strings.")
+
+    for postcode in postcodes:
+        if not isinstance(postcode, str):
+            raise TypeError("Function expects a list of strings.")
+
+    res = req.post(f"https://api.postcodes.io/postcodes")
+    if res.status_code == 200:
+        return res.json()["result"]
+
+    if res.status_code == 500:
+        raise req.RequestException("Unable to access API.")
+
+
+if __name__ == "__main__":
+    print(get_postcode_for_location(51.507, -0.127))
